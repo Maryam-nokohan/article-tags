42a3f109e76bcf782bef1098284e785b3cf18ba1
mary <maryam.nokohan@gmail.com>
2026-02-22 13:45:50 +0330

    feat : create the article service

--- internal/application/article_service.go
+++ internal/application/article_service.go
@@ -2,29 +2,37 @@ package application
 
 import (
 	"context"
+	"time"
 
 	"github.com/maryam-nokohan/go-article/internal/domain"
+	"github.com/maryam-nokohan/go-article/internal/ports"
 )
 
 type ArticleService struct {
-	repo      domain.ArticleRepository
+	repo      ports.ArticleRepository
 	extractor domain.TagExtractor
 }
 
-func NewArticleService(r domain.ArticleRepository, e domain.TagExtractor) *ArticleService {
+func NewArticleService(r ports.ArticleRepository, e domain.TagExtractor) *ArticleService {
 	return &ArticleService{
 		repo:      r,
 		extractor: e,
 	}
 }
 
-func (s *ArticleService) ProcessArticle(ctx context.Context , articles []domain.Article) (error){
-
-	return  nil
+func (s *ArticleService) ProcessArticle(articles domain.Article) (error){
+	
+		tagWords := s.extractor.Extract(articles.Body , -1) 
+		articles.Tags = tagWords
+		articles.Created_at = time.Now()		
+		err := s.repo.Save(context.Background() , &articles)
+	return  err
 }
 
 func (s *ArticleService) GetTopTags(ctx context.Context , limit int64)([]domain.Tag , error){
-
-	
-	return  nil , nil
+	tags , err := s.repo.GetTopTags(ctx , limit)
+	if err != nil {
+		return  nil , err
+	}
+	return  tags , nil
 }
